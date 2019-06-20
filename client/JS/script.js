7bac66d7e1af9045f1975aa084e0532fd75d92a4
Tom Becker <tjbecker@me.com>
2019-06-20 16:32:05 +0700

    added forEach loop for sidebar

--- client/JS/script.js
+++ client/JS/script.js
@@ -4,8 +4,16 @@ window.onload = ()=>{
 	// let sidebar = document.getElementById('sidebar')
 	// sidebar.insertAdjacentHTML('beforeBegin', `<h1>'hey'</h1>`)
 	let ul = document.getElementsByTagName('ul')[0]
-	ul.insertAdjacentHTML('beforeEnd', `
-	<li>
-		<a href="#">wallets</a>
-	</li>` )
+	let categories = ['wallets', 'bags', 'shoes', 'Umbrellas']
+	categories.forEach((c) =>{
+		ul.insertAdjacentHTML('beforeEnd', `
+		<li>
+			<a href="#">${c}</a>
+		</li>` )
+	})
+	// let ul = document.getElementsByTagName('ul')[0]
+	// ul.insertAdjacentHTML('beforeEnd', `
+	// <li>
+	// 	<a href="#">wallets</a>
+	// </li>` )
 	}
