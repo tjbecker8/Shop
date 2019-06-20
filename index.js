b61472bbe8f768a3e58ee029788b00e7e2413392
Tom Becker <tjbecker@me.com>
2019-06-20 10:52:38 +0700

    my work

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+//database
+const db = require('./db')
+
 // Static Files
 
 // serve every file inside 'client' folder as static
@@ -12,6 +15,26 @@ app.use(express.static(path.join(__dirname, 'client')))
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/client/products.html'))
 	console.log('__dirname', __dirname)
+})
+
+app.get('/products', (req, res) => {
+	db.query(`SELECT * FROM products`, (err, result) => {
+		if (err) {
+			console.log('err', err)
+		} else {
+			console.log('result', result.rows)
+		}
+	})
+})
+
+app.get('/categories', (req, res) => {
+	db.query(`SELECT * FROM categories`, (err, result) => {
+		if (err) {
+			console.log('err', err)
+		} else {
+			console.log('result', result)
+		}
+	})
 })
 
 // Run Server
